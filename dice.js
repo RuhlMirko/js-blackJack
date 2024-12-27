fa8795f044e67d5a17f92775c7dd6648e9b78592
Mirko <ruhlmirkojoel@gmail.com>
2024-12-26 22:08:25 -0300

    Testing dom manipulation

--- dice.js
+++ dice.js
@@ -1,25 +1,17 @@
 "use strict";
-console.log("hello");
 
-/*
-const enemies = {
-  player: "e1",
-  dice_amount: 5,
-  e1_dices: Array.from({ length: 5 }, () => Math.floor(Math.random() * 6)),
-};*/
-
+// Constructor
 const enemiesObj = (player, dice_amount = 5, dices) => ({
   player,
   dice_amount,
   dices,
 });
-
 function getNewDices() {
   return Array.from({ length: 5 }, () => Math.floor(Math.random() * 6) + 1);
 }
-
+// Enemies object creation
 const enemies = [
-  enemiesObj("e1", 5, getNewDices()),
+  enemiesObj("e1", 2, getNewDices()),
   enemiesObj("e2", 5, getNewDices()),
   enemiesObj("e3", 5, getNewDices()),
   enemiesObj("e4", 5, getNewDices()),
@@ -30,3 +22,8 @@ const mapedEnemiesDices = enemies.map(function (item) {
   return [...item.dices];
 });
 console.log(mapedEnemiesDices);
+
+// DOM manipulation
+/*
+document.querySelector(".e1").textContent = enemies[0].dice_amount;
+*/
