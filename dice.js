53517de81009a650c3471eb7fdca1370bf4b4a88
Mirko <ruhlmirkojoel@gmail.com>
2024-12-26 21:52:34 -0300

    Initial commit of game logic

--- dice.js
+++ dice.js
@@ -1,4 +1,29 @@
 "use strict";
 console.log("hello");
 
-document.querySelector();
+/*
+const enemies = {
+  player: "e1",
+  dice_amount: 5,
+  e1_dices: Array.from({ length: 5 }, () => Math.floor(Math.random() * 6)),
+};*/
+
+const enemiesObj = (player, dice_amount = 5, dices) => ({
+  player,
+  dice_amount,
+  dices,
+});
+
+function getNewDices() {
+  return Array.from({ length: 5 }, () => Math.floor(Math.random() * 6) + 1);
+}
+
+const enemies = [
+  enemiesObj("e1", 5, getNewDices()),
+  enemiesObj("e2", 5, getNewDices()),
+  enemiesObj("e3", 5, getNewDices()),
+  enemiesObj("e4", 5, getNewDices()),
+  enemiesObj("e5", 5, getNewDices()),
+];
+
+console.log(enemies);
