9147dd5e32cf9c4b256241d18df726b162910c37
Mirko <ruhlmirkojoel@gmail.com>
2024-12-28 11:48:15 -0300

    Added console log to print all the dices numbers

--- dice.js
+++ dice.js
@@ -18,17 +18,24 @@ const enemies = [
   enemiesObj(5, 5, getNewDices()),
 ];
 
-const mapedEnemiesDices = enemies.map(function (item) {
+const allDices = enemies.map(function (item) {
   return [...item.dices];
 });
-//console.log(mapedEnemiesDices);
+console.log(
+  ` Dices:
+  p1: ${allDices[0]}
+  p2: ${allDices[1]}
+  p3: ${allDices[2]}
+  p4: ${allDices[3]}
+  p5: ${allDices[4]}`
+);
 
-// DOM manipulation
-/*
+/* DOM manipulation
 document.querySelector(".e1").textContent = enemies[0].dice_amount;
-*/
 
-console.log(enemies[0].dices);
+
+
+// Sets html to reflect the player dices
 let fullString = "";
 for (let i = 0; i < 5; i++) {
   const currentNum = enemies[0].dices[i];
@@ -36,9 +43,4 @@ for (let i = 0; i < 5; i++) {
   fullString += `<img src="res/dice--${currentNum}.png" alt="" class="p1-d0${i}" />\n`;
 }
 document.querySelector(`.current-hand`).innerHTML = fullString;
-
-const testArr = [
-  [4, 5, 6],
-  [7, 8, 9],
-];
-//console.log(testArr[0][0]);
+*/
